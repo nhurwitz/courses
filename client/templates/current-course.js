2976c017b0c834692a903f9093c426e3af713cfc
Noam Hurwitz <nhurwitz@gmail.com>
2014-12-27 14:02:11 -0800

    added icon

--- client/templates/current-course.js
+++ client/templates/current-course.js
@@ -2,5 +2,13 @@ Template.currentCourse.helpers({
   name: function() {
     var course = Courses.find({_id:Session.get(CURRENT_COURSE_VIEW)}).fetch()[0];
     return course.name;
+  },
+  dept: function() {
+    var course = Courses.find({_id:Session.get(CURRENT_COURSE_VIEW)}).fetch()[0];
+    return course.department;
+  },
+  courseNumber: function() {
+    var course = Courses.find({_id:Session.get(CURRENT_COURSE_VIEW)}).fetch()[0];
+    return course.courseNumber;
   }
 });_
